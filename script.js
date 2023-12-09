3e5c3a6bf0f709f7c2cb06f2275acba48608be1d
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-12-09 17:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let str = navigator.userAgent;
+let brouserName = "";
+if(str.includes("Chrome")) {
+    browserName = "Chrome";
+} else if(str.includes("Firefox")) {
+    browserName = "Firefox";
+} else if(str.includes("Safari")) {
+    browserName = "Safari";
+} else if(str.includes("Opera")) {
+    browserName = "Opera";
+} else if(str.includes("MSIE")) {
+    browserName = "Internet Explorer";
+}
+
+let VersionArr = str.split(browserName);
+
+let version = VersionArr[1].split(" ")[0];
+
+let Div = documnet.createElement("div");
+Div.id = "browser-info";
+Div.innerText = "You are using " + browserName + " version " + version;
+
